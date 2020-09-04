ac86b9b956ddde6bde19fc6dfbdfbd559a184ea1
asimon <asimon@billtrust.com>
2020-09-03 21:43:38 -0400

    landing page completed, working on rules page

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     var navbar = document.getElementById("navbar");
     navbar.style.display = "none";
-})
 
-new WOW().init();
+    var toggle_icon = document.getElementById('toggler');
+    toggler.style.display = "none";
+
+})
